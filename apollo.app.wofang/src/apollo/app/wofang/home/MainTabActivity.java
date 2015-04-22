00ee1eb1c2e073f212741a9080386b4c85e649b4
kuibobo <kuibobo@gmail.com>
2015-04-22 21:07:11 +0800

    简单设置布局
    #1

--- apollo.app.wofang/src/apollo/app/wofang/home/MainTabActivity.java
+++ apollo.app.wofang/src/apollo/app/wofang/home/MainTabActivity.java
@@ -1,7 +1,32 @@
 package apollo.app.wofang.home;
 
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.View;
 import apollo.app.BaseActivity;
+import apollo.app.wofang.R;
 
 public class MainTabActivity extends BaseActivity {
 
+	private ViewPager mViewPager;
+	private View mView;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.setContentView(R.layout.activity_maintabs);
+		
+		initView();
+		initListener();
+	}
+	
+	
+	private void initView() {
+		mViewPager = (ViewPager)super.findViewById(R.id.view_pager);
+		mView = (View) super.findViewById(R.id.view);
+	}
+	
+	private void initListener() {
+		
+	}
 }
