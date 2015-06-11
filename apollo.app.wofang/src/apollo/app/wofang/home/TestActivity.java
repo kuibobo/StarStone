6c810c3daebf8a471ed2d115e19239f834ab13b8
kuibobo <kuibobo@gmail.com>
2015-06-11 20:51:06 +0800

    简单修改，加入滑动菜单。
    #2

--- apollo.app.wofang/src/apollo/app/wofang/home/TestActivity.java
+++ apollo.app.wofang/src/apollo/app/wofang/home/TestActivity.java
@@ -1,83 +1,157 @@
 package apollo.app.wofang.home;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import apollo.app.BaseActivity;
 import apollo.app.wofang.R;
-import apollo.model.Section;
-import apollo.view.DragAdapter;
-import apollo.view.DragGridView;
 
 public class TestActivity extends BaseActivity {
 
-	private DragGridView mDragGridViewCurrent = null;
-	private DragGridView mDragGridViewSource = null;
+	private ActionBarDrawerToggle mDrawerToggle;
 
-	private List<Section> mSectionsCurrent = null;
-	private List<Section> mSectionsSource = null;
+	private DrawerLayout mLayoutMain = null;
+	private View mLayoutLeft = null;
+	private View mLayoutRight; // 右滑视图
 
-	private DragAdapter mSectionAdapterCurrent = null;
-	private DragAdapter mSectionAdapterSource = null;
+	private CharSequence mDrawerTitle;
+	private CharSequence mTitle;
 
 	public static void startActivity(Context context) {
 		Intent intent = null;
-		
+
 		intent = new Intent(context, TestActivity.class);
 		context.startActivity(intent);
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		super.setContentView(R.layout.activity_test);
 
-		this.mSectionsCurrent = new ArrayList<Section>();
-		this.mSectionsSource = new ArrayList<Section>();
-		
 		this.initView();
 		this.initListener();
 
-		// test code
-
-		for (int i = 0; i < 60; i++) {
-			Section s = new Section();
-			s.setId(i);
-			s.setName("Section" + i);
-
-			this.mSectionsCurrent.add(s);
-		}
-		this.mSectionAdapterCurrent.notifyDataSetChanged();
-		// end test code
-
-		// test code
-
-		for (int i = 0; i < 64; i++) {
-			Section s = new Section();
-			s.setId(i);
-			s.setName("Section" + i);
-
-			this.mSectionsSource.add(s);
-		}
-		this.mSectionAdapterSource.notifyDataSetChanged();
-		// end test code
 	}
 
 	@SuppressLint("InflateParams")
 	private void initView() {
-		this.mSectionAdapterCurrent = new DragAdapter(this, this.mSectionsCurrent);
-		this.mSectionAdapterSource = new DragAdapter(this, this.mSectionsSource);
-		
-		this.mDragGridViewCurrent = (DragGridView) super.findViewById(R.id.grid_current);
-		this.mDragGridViewCurrent.setAdapter(this.mSectionAdapterCurrent);
-		
-		this.mDragGridViewSource = (DragGridView) super.findViewById(R.id.grid_source);
-		this.mDragGridViewSource.setAdapter(this.mSectionAdapterSource);
+		mTitle = mDrawerTitle = getTitle();
+
+		this.mLayoutMain = (DrawerLayout) super.findViewById(R.id.layout_main);
+		this.mLayoutLeft = (View) super.findViewById(R.id.layout_left);
+		this.mLayoutRight = (View) super.findViewById(R.id.layout_right);
+
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+		getActionBar().setDisplayShowHomeEnabled(false); // 隐藏系统actiobar icon
+		getActionBar().setDisplayShowTitleEnabled(false);
+		getActionBar().setDisplayShowCustomEnabled(true);
+		View actionbarLayout = LayoutInflater.from(this).inflate(
+				R.layout.item_layout_action_bar, null);
+
+		getActionBar().setCustomView(actionbarLayout);
+		Button leftbtn = (Button) actionbarLayout.findViewById(R.id.btn_left); // 左边按钮
+		Button Rightbtn = (Button) actionbarLayout.findViewById(R.id.btn_right); // 右边按钮
+
+		leftbtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// 如果右边视图显示则关闭，如果左边视图显示则关闭如果关闭则显示
+				if (mLayoutMain.isDrawerOpen(mLayoutRight)) {
+					mLayoutMain.closeDrawer(Gravity.END);
+				}
+				if (mLayoutMain.isDrawerOpen(mLayoutLeft)) {
+					mLayoutMain.closeDrawer(Gravity.START);
+				} else {
+					mLayoutMain.openDrawer(Gravity.START);
+
+				}
+			}
+		});
+		Rightbtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// 如果左边视图显示则关闭，如果右边视图显示则关闭如果关闭则显示
+				if (mLayoutMain.isDrawerOpen(mLayoutLeft)) {
+					mLayoutMain.closeDrawer(Gravity.START);
+				}
+				if (mLayoutMain.isDrawerOpen(mLayoutRight)) {
+					mLayoutMain.closeDrawer(Gravity.END);
+				} else {
+					mLayoutMain.openDrawer(Gravity.END);
+
+				}
+
+				// TODO Auto-generated method stub
+
+			}
+		});
 	}
 
-	private void initListener() {}
+	@SuppressWarnings("deprecation")
+	private void initListener() {
+		this.mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
+		this.mLayoutMain, /* DrawerLayout object */
+		R.drawable.ic_drawer, /* nav drawer image to replace 'Up' caret */
+		R.string.drawer_open, /* "open drawer" description for accessibility */
+		R.string.drawer_close /* "close drawer" description for accessibility */
+		) {
+
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu(); // creates call to
+											// onPrepareOptionsMenu()
+			}
+		};
+
+		this.mLayoutMain.setDrawerListener(this.mDrawerToggle);
+	}
+
+	/* Called whenever we call invalidateOptionsMenu() */
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// If the nav drawer is open, hide action items related to the content
+		// view
+		boolean drawerOpen = this.mLayoutMain.isDrawerOpen(this.mLayoutLeft);
+		// menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle your other action bar items...
+
+		return super.onOptionsItemSelected(item);
+	}
 }
---
+++ apollo.app.wofang/src/apollo/widget/NavigationDrawerFragment.java
@@ -0,0 +1,120 @@
+package apollo.widget;
+
+import android.app.ActionBar;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import apollo.app.wofang.R;
+
+public class NavigationDrawerFragment extends Fragment {
+
+	/**
+	 * Remember the position of the selected item.
+	 */
+	private static final String STATE_SELECTED_POSITION = "selected_navigation_drawer_position";
+
+	/**
+	 * Per the design guidelines, you should show the drawer on launch until the
+	 * user manually expands it. This shared preference tracks this.
+	 */
+	private static final String PREF_USER_LEARNED_DRAWER = "navigation_drawer_learned";
+
+	/**
+	 * A pointer to the current callbacks instance (the Activity).
+	 */
+	//private NavigationDrawerCallbacks mCallbacks;
+
+	/**
+	 * Helper component that ties the action bar to the navigation drawer.
+	 */
+	private ActionBarDrawerToggle mDrawerToggle;
+	private DrawerLayout mDrawerLayout;
+	private View mDrawerView;
+	private View mFragmentContainerView;
+
+	private int mCurrentSelectedPosition = 0;
+	private boolean mFromSavedInstanceState;
+	private boolean mUserLearnedDrawer;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(
+				R.layout.item_layout_fragment_navigation_drawer, container,
+				false);
+	}
+
+	public void setUp(int fragmentId, DrawerLayout drawerLayout) {
+		this.mFragmentContainerView = getActivity().findViewById(fragmentId);
+		this.mDrawerLayout = drawerLayout;
+
+		this.mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow,
+				GravityCompat.START);
+
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setHomeButtonEnabled(true);
+
+		mDrawerToggle = new ActionBarDrawerToggle(getActivity(), /* host Activity */
+		mDrawerLayout, /* DrawerLayout object */
+		R.drawable.ic_drawer, /* nav drawer image to replace 'Up' caret */
+		R.string.drawer_open, /*
+										 * "open drawer" description for
+										 * accessibility
+										 */
+		R.string.drawer_close /*
+										 * "close drawer" description for
+										 * accessibility
+										 */
+		) {
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				super.onDrawerClosed(drawerView);
+				if (!isAdded()) {
+					return;
+				}
+
+				getActivity().invalidateOptionsMenu(); // calls
+														// onPrepareOptionsMenu()
+			}
+
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+				if (!isAdded()) {
+					return;
+				}
+
+				if (!mUserLearnedDrawer) {
+					// The user manually opened the drawer; store this flag to
+					// prevent auto-showing
+					// the navigation drawer automatically in the future.
+					mUserLearnedDrawer = true;
+					SharedPreferences sp = PreferenceManager
+							.getDefaultSharedPreferences(getActivity());
+					sp.edit().putBoolean(PREF_USER_LEARNED_DRAWER, true)
+							.apply();
+				}
+
+				getActivity().invalidateOptionsMenu(); // calls
+														// onPrepareOptionsMenu()
+			}
+		};
+	}
+	
+	private ActionBar getActionBar() {
+		return getActivity().getActionBar();
+	}
+}
