d4dd8a9c115c5162e10b5ddd1f348cda202f6b16
kuibobo <kuibobo@gmail.com>
2015-08-06 17:52:52 +0800

    简单更新.

---
+++ app/src/main/java/apollo/app/wofang/activity/ContentViewPagerFragment.java
@@ -0,0 +1,44 @@
+package apollo.app.wofang.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import apollo.app.wofang.R;
+
+/**
+ * Created by Texel on 2015/8/6.
+ */
+public class ContentViewPagerFragment extends Fragment implements
+        ViewPager.OnPageChangeListener {
+
+    private ViewPager mViewPager;
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = null;
+
+        mViewPager = (ViewPager) view.findViewById(R.id.main_tab_pager);
+        return view;
+    }
+}
